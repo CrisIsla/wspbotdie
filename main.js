11e8a2543d906aa523a53f010be3b7d8c8b5db1c
CrisIsla <cristobal.isla@ug.uchile.cl>
2024-05-01 16:18:10 -0400

    add basic survey logic

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { Client } = require('whatsapp-web.js'); 
 const qrcode = require('qrcode-terminal')
 
+const questions = ["Question1", "Question2", "Question3", "Thanks for your time"]
+
 // Create a new client instance
 const client = new Client();
 
@@ -17,11 +19,33 @@ client.on('qr', (qr) => {
 // Listening to all incoming messages
 client.on('message_create', message => {
     async function getChat() {
-        var chat = (await message.getChat()).fetchMessages();
-        var messages = (await chat).map((msg) => msg.body);
-        return messages;
+        var chat = await message.getChat();
+        var messages = await chat.fetchMessages();
+        var messages_body = (await messages).map((msg) => msg.body);
+        return messages_body;
     }
-    messages = getChat()
+
+    async function surveyLogic() {
+        if (!message.fromMe){
+            const messages = await getChat();
+            // console.log(messages)
+            
+            for(let i=0; i<questions.length; i++){
+                if(!messages.includes(questions[i])){
+                    console.log("Se envio el mensaje")
+                    client.sendMessage(message.from, questions[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    async function clearChat() {
+        var chat = await message.getChat();
+        await chat.clearMessages();
+        console.log(chat);
+    } 
+    surveyLogic();
 });
 
 // Start your client
