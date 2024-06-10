77721ae407fa41eb150e76d4a7ba8e591710b65b
CrisIsla <cristobal.isla@ug.uchile.cl>
2024-06-10 13:19:56 -0400

    add sent first message status to answers dict. refactor surveyLogic

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ client.once("ready", () => {
     for (let i = 0; i < numbers.length; i++) {
       client.sendMessage(numbers[i], bot_messages["welcome-message"]);
       answers[numbers[i]] = {
+        sent_first_question: false,
         answers: [],
         is_done: false,
       };
@@ -95,27 +96,34 @@ client.on("message", (message) => {
   function surveyLogic() {
     if (message.body == "" || answers[message.from].is_done) return;
     console.log("Message received:", message.body);
-    total_answers = answers[message.from].answers.length;
-    if (total_answers == 0) {
-      last_question = bot_messages["welcome-message"];
-    } else {
-      last_question = bot_messages.questions[total_answers - 1];
-    }
     let response;
-    if (total_answers == TOTAL_QUESTIONS) {
-      response = bot_messages["end-message"];
-      answers[message.from].is_done = true;
-    } else if (
-      last_question == bot_messages["welcome-message"] ||
-      last_question.type == "text" ||
-      getQuestionChoices(last_question.options).includes(
+    if (!answers[message.from].sent_first_question) {
+      response = formulateQuestion(bot_messages.questions[0]);
+      client.sendMessage(message.from, response);
+      console.log(message.from);
+      console.log("Message sent:", response);
+      answers[message.from].sent_first_question = true;
+      return;
+    }
+    total_answers = answers[message.from].answers.length;
+    last_question = bot_messages.questions[total_answers];
+    if (
+      last_question.type != "text" &&
+      !getQuestionChoices(last_question.options).includes(
         message.body.toLowerCase()
       )
-    ) {
-      response = formulateQuestion(bot_messages.questions[total_answers]);
-      answers[message.from].answers.push(message.body);
-    } else {
+    )
       response = bot_messages.invalid;
+    else {
+      if (last_question === bot_messages.questions[TOTAL_QUESTIONS - 1]) {
+        response = bot_messages["end-message"];
+        answers[message.from].answers.push(message.body);
+        answers[message.from].is_done = true;
+      } else {
+        total_answers += 1;
+        response = formulateQuestion(bot_messages.questions[total_answers]);
+        answers[message.from].answers.push(message.body);
+      }
     }
     client.sendMessage(message.from, response);
     console.log(message.from);
